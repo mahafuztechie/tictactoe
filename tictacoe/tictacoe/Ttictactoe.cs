e55207cca08691094370b2ebc1dec3ebbf6ccd62
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-03 10:46:40 +0530

    [mahafuz] adding createboard

--- tictacoe/tictacoe/Ttictactoe.cs
+++ tictacoe/tictacoe/Ttictactoe.cs
@@ -11,7 +11,7 @@ namespace Tictacoe
         public char[] CreateBoard()
         {
             char[] board = new char[10];
-            for(int i = 0; i<board.Length; i++)
+            for(int i = 1; i<board.Length; i++)
             {
                 board[i] = ' ';
             }
